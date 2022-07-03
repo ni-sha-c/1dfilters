8e9526d941a0da8d471f93ac726ee7f1109d48ae
Nisha Chandramoorthy <nishac@mit.edu>
2022-07-03 05:03:42 -0400

    Outline for 1dtransport

--- src/transport_1D.jl
+++ src/transport_1D.jl
@@ -11,64 +11,49 @@ function generate_data(T, s)
 	end
 	return x, y
 end
-function resample(x,w)
-    cdfw = cumsum(w)
-    new_pts = similar(x)
-	N_p = size(x)[1]
-    for j = 1:N_p
-        r = rand()
-        for i = 1:N_p
-		    if cdfw[i] >= r
-                new_pts[j] = x[i]
-			    break
-			end
-		end
-    end
-	return new_pts
-end
 function p_y_g_x(a)
 		return log(1/sqrt(2π)/σ_o) -0.5*a*a/σ_o/σ_o
 end
-function analysis(w, y, x)
-    N_p = size(x)[1]
-	lw = log.(w)
-	for k = 1:N_p
-		lw[k] = lw[k] + p_y_g_x(y - x[k]) 
-	end
-	w1 = exp.(lw)
-	w1 ./= sum(w1)
-	return w1
+function cdf(x, xarr)
+	N = size(xarr)[1]
+	return 1/N*sum(xarr .<= x)
 end
+function posterior(xarr, y)
+	N = size(xarr)[1]
+	a_n = y .- xarr
+	lklhd = p_y_g_x.(a_n)
+	prob = (1/N)*lklhd
+	prob ./= sum(prob)
+	return prob
+end
+function rearrange(x_pr, post_pr)
+	x_post = 
+
+end
+function analysis(y, x)
+	
+end
+
+
 function forecast(x, s, τ)
 	for t = 1:τ
 		x .= next.(x, s)
 	end
 	return x
 end
-
-function sir(x,y,w,τ,T,N,N_thr,s)
+function transport_filter(x,y,τ,T,N,s)
 	x_pr = zeros(N,T)
-	w_pr = zeros(N,T)
 	for t = 1:T
 		x .= forecast(x, s, τ)
-		w .= analysis(w, y[t], x)
-		N_eff = 1.0/sum(w.*w)
-        if (N_eff < N_thr)
-			x .= resample(x,w)
-	    end
+		x .= analysis(y[t], x)
 		x_pr[:,t] .= x
-		w_pr[:,t] .= w
     end
-	return x_pr, w_pr
+	return x_pr
 end
 function assimilate(N, Nthr, τ, T, s)
 	x_t, y = generate_data(T, s)
 	x = rand(N)
 	x .= forecast(x, s, 1000)
-	w = ones(N)/N
-	x_pr, w_pr = sir(x,y,w,τ,T,N,Nthr,s)
-	return x_pr, w_pr, x_t, y 
+	x_pr = transport_filter(x,y,τ,T,N,s)
+	return x_pr, x_t, y 
 end
-
-
-
